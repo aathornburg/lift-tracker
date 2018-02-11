d70cbe8283d1d5e6afc2ca1870aaddb58bdb57e6
aathornburg <alan.thornb@gmail.com>
2018-02-11 18:18:21 -0500

    Moved Notes to OneNote.  Changed AngularJS/ui-router to come from CDN.  NEED http-server FROM NPM TO RUN.

---
+++ app/app.js
@@ -0,0 +1,11 @@
+var app = angular.module('app', ['ui.router']);
+
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+        .state('/home', {
+            url: '/home',
+            templateUrl: 'home.html'
+        });
+}]);
