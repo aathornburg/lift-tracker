73e3532b2cb992507b039be9d4a5ca6f0e71a6a5
aathornburg <alan.thornb@gmail.com>
2018-02-11 19:36:30 -0500

    Updated application to run on a server using http-server.  Added more AngularJS code and started on a factory

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 The app is running on a simple http-server (npm install -g http-server).
 
-To start the server, run "http-server -o" (-o means open)
+To start the server, run "http-server -a localhost -p 8080 -c-1"
--- app/app.js
+++ app/app.js
@@ -1,11 +1,33 @@
 var app = angular.module('app', ['ui.router']);
 
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/inbox');
 
     $stateProvider
-        .state('/home', {
-            url: '/home',
-            templateUrl: 'home.html'
+        .state('/inbox', {
+            url: '/inbox',
+            templateUrl: 'inbox.html',
+            controller: 'InboxCtrl'
+        })
+        .state('/inbox/email/:id', {
+            url: '/inbox/email/:id',
+            templateUrl: 'views/email.html',
+            controller: 'EmailCtrl',
+            controllerAs: 'email'
         });
 }]);
+
+app.controller('InboxCtrl', function($scope) {
+    $scope.title = "This is a title";
+});
+
+app.factory('InboxFactory', function($http) {
+    var exports = {};
+
+    exports.getMessages = function() {
+        return $http.get('emails.json')
+            .error(function (data) {
+                console.log('There was an error!', data);
+            });
+    };
+});
