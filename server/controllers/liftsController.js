338103b35bfd318a2c80b3342378e23f8e2c73f0
aathornburg <alan.thornb@gmail.com>
2018-02-27 20:06:58 -0500

    Retrieving all lifts from the DB.  Yay!

--- server/controllers/liftsController.js
+++ server/controllers/liftsController.js
@@ -5,19 +5,27 @@ export default {
     get: (req, res, next) => {
         // TODO:  Try to hit this from the front end
         console.log("I hit this from the front end!");
-        res.send("Yay!");
+        lift.find((err, lifts) => {
+            if (err) {
+                res.send(err);
+            }
+
+            res.json({lifts: lifts});
+        });
     },
     put: (req, res, next) => {
-        console.log("Trying to put a lift into the DB...");
+        console.log("Trying to add lift: " + req.params['lift'] + " @ " + req.params['weight'] + " lbs");
         lift.create(
             {
-                lift: req.body.lift,
-                weight: req.body.weight
+                lift: req.params['lift'],
+                weight: req.params['weight']
             },
             (err, errInstance) => {
                 if (err) {
-                    console.log(err);
+                    res.send(err);
                 }
+
+                res.send("Success?");
             }
         );
     }
--- src/app/services/LiftsService.js
+++ src/app/services/LiftsService.js
@@ -4,10 +4,8 @@ export class LiftsService {
     /* @ngInject */
     constructor($http) {
         this.$http = $http;
-        this.lifts = [
-            new Lift("Bench press", 145)
-        ];
-        /** DB logic here */
+
+        this.storeAllLifts();
     }
 
     /** public methods */
@@ -17,13 +15,24 @@ export class LiftsService {
 
     storeLift(lift, weight) {
         this.$http.put('/api/lifts?lift=' + lift + '&weight=' + weight)
-            .success((data, status, headers) => {
+            .then((data, status, headers) => {
                 alert("success!");
                 this.lifts.push(new Lift(lift, weight));
             })
-            .error((data, status, headers) => {
+            .catch((data, status, headers) => {
                 console.log("DB write failure");
                 // TODO:  Pop up error for user
+            });
+    }
+
+    storeAllLifts() {
+        this.$http.get('/api/lifts')
+            .then((data, status, headers) => {
+                this.lifts = data.data.lifts;
+            })
+            .catch((data, status, headers) => {
+                // TODO:  Failure scenario
+                console.log("Lifts were not received :(");
             });
     }
 }
