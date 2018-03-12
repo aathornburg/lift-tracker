c0f9aae75a7208d3ad4f75837bd509b08ed53ee8
aathornburg <alan.thornb@gmail.com>
2018-03-11 20:44:59 -0400

    Organized lift and workout components to clean up components folder

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -9,12 +9,12 @@ import { servicesModule } from './services/services.module';
 /** Components */
 import { navigationModule } from './components/navigation/navigation.module';
 import { dashboardModule } from './components/dashboard/dashboard.module';
-import { workoutsModule } from './components/workouts/workouts.module';
-import { liftsModule } from './components/lifts/lifts.module';
+import { workoutsModule } from './components/workout-components/workouts/workouts.module';
+import { liftsModule } from './components/lift-components/lifts/lifts.module';
 import { goalsModule } from './components/goals/goals.module';
-import { addLiftWeightModule } from './components/add-lift-weight/addLiftWeight.module';
-import { allLiftsModule } from './components/all-lifts/allLifts.module';
-import { createFirstWorkoutModule } from './components/create-first-workout/createFirstWorkout.module';
+import { addLiftWeightModule } from './components/lift-components/add-lift-weight/addLiftWeight.module';
+import { allLiftsModule } from './components/lift-components/all-lifts/allLifts.module';
+import { createFirstWorkoutModule } from './components/workout-components/create-first-workout/createFirstWorkout.module';
 
 export default angular
     .module('angularJSSandbox',
