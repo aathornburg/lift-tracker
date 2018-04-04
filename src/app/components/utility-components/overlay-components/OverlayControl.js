0dd6bd64d10655ed559c72ce1590c1b1595ceae1
aathornburg <alan.thornb@gmail.com>
2018-04-04 17:34:39 -0400

    Updated look of modal and corrected issue with tab key locking

--- src/app/components/utility-components/overlay-components/OverlayControl.js
+++ src/app/components/utility-components/overlay-components/OverlayControl.js
@@ -5,120 +5,133 @@ export class OverlayControl {
 
     createPublicOverlayControlFunctions() {
         return (() => {
-            const methods = {
-                generateNamespacedEvent: (eventType, id) => {
-                    return eventType + '.' + id;
-                },
-                elementOutsideContainer: ($element, $container) => {
-                    return !($container.has($element).length)
-                        && !($container.is($element)); // This is an inclusive check
-                },
-                event: {
-                    getRelatedElement: (e) => {
-                        if (e.relatedTarget !== null) { /* Chrome and FF support relatedTarget on focus out */
-                            return $(e.relatedTarget);
-                        } else { /* IE doesn't support relatedTarget but supports document.activeElement */
-                            return $(document.activeElement);
+            const constants = {
+                tabKeyScenarios: [
+                    { // Leaving main element forwards
+                        check: (e, $element, focusableItems) => methods.tabKey.leavingFromElement($element[0]) && !e.shiftKey,
+                        getDestination: (focusableItems) => focusableItems[0]
+                    },
+                    { // Leaving main element backwards
+                        check: (e, $element, $focusableItems) => methods.tabKey.leavingFromElement($element[0]) && e.shiftKey,
+                        getDestination: (focusableItems) => focusableItems[focusableItems.length - 1]
+                    },
+                    { // Leaving first focusable element backwards
+                        check: (e, $element, focusableItems) => methods.tabKey.leavingFromElement(focusableItems[0]) && e.shiftKey,
+                        getDestination: (focusableItems) => focusableItems[focusableItems.length - 1]
+                    },
+                    { // Leaving last focusable element forwards
+                        check: (e, $element, focusableItems) => methods.tabKey.leavingFromElement(focusableItems[focusableItems.length - 1]) && !e.shiftKey,
+                        getDestination: (focusableItems) => focusableItems[0]
+                    }
+                ],
+            },
+                methods = {
+                    generateNamespacedEvent: (eventType, id) => {
+                        return eventType + '.' + id;
+                    },
+                    elementOutsideContainer: ($element, $container) => {
+                        return !($container.has($element).length)
+                            && !($container.is($element)); // This is an inclusive check
+                    },
+                    event: {
+                        getRelatedElement: (e) => {
+                            if (e.relatedTarget !== null) { /* Chrome and FF support relatedTarget on focus out */
+                                return $(e.relatedTarget);
+                            } else { /* IE doesn't support relatedTarget but supports document.activeElement */
+                                return $(document.activeElement);
+                            }
+                        },
+                        getTargetElement: (e) => {
+                            return $(e.target);
                         }
                     },
-                    getTargetElement: (e) => {
-                        return $(e.target);
-                    }
-                },
-                focus: {
-                    leavingElement: {
-                        createListener: (id, $element, callback) => {
+                    focus: {
+                        leavingElement: {
+                            createListener: (id, $element, callback) => {
+                                $element.on(
+                                    methods.generateNamespacedEvent('focusout', id),
+                                    (e) => {
+                                        if (methods.elementOutsideContainer(methods.event.getRelatedElement(e), $element)) {
+                                            callback();
+                                        }
+                                    }
+                                );
+                            },
+                            removeListener: (id, $element) => {
+                                $element.off(
+                                    methods.generateNamespacedEvent('focusout', id)
+                                );
+                            }
+                        }
+                    },
+                    click: {
+                        leavingElement: {
+                            createListener: (id, $element, callback) => {
+                                $(document).on(
+                                    methods.generateNamespacedEvent('click', id),
+                                    (e) => {
+                                        if (methods.elementOutsideContainer(methods.event.getTargetElement(e), $element)) {
+                                            callback();
+                                        }
+                                    }
+                                );
+                            },
+                            removeListener: (id, $element) => {
+                                $(document).off(
+                                    methods.generateNamespacedEvent('click', id)
+                                );
+                            }
+                        }
+                    },
+                    tabKey: {
+                        leavingFromElement: (element) => {
+                            return document.activeElement === element;
+                        },
+                        tabToCorrectElement: (e, $element, focusableItems) => {
+                            let scenario = constants.tabKeyScenarios.find(scenario => scenario.check(e, $element, focusableItems));
+                            if (scenario) {
+                                e.preventDefault();
+                                scenario.getDestination(focusableItems).focus();
+                            }
+                        },
+                        trap: (id, $element) => {
+                            let focusableItems = $element.find(':focusable');  // Need jQuery UI for this
+                            $element.focus();
+        
                             $element.on(
-                                methods.generateNamespacedEvent('focusout', id),
+                                methods.generateNamespacedEvent('keydown', id),
                                 (e) => {
-                                    if (methods.elementOutsideContainer(methods.event.getRelatedElement(e), $element)) {
-                                        callback();
+                                    if (e.keyCode === 9) {
+                                        methods.tabKey.tabToCorrectElement(e, $element, focusableItems);
                                     }
                                 }
                             );
                         },
-                        removeListener: (id, $element) => {
+                        removeTrap: (id, $element) => {
                             $element.off(
-                                methods.generateNamespacedEvent('focusout', id)
+                                methods.generateNamespacedEvent('keydown', id)
                             );
                         }
-                    }
-                },
-                click: {
-                    leavingElement: {
-                        createListener: (id, $element, callback) => {
+                    },
+                    escapeKey: {
+                        createListener: (id, callback) => {
                             $(document).on(
-                                methods.generateNamespacedEvent('click', id),
+                                methods.generateNamespacedEvent('keydown', id),
                                 (e) => {
-                                    if (methods.elementOutsideContainer(methods.event.getTargetElement(e), $element)) {
+                                    if (e.keyCode === 27) {
                                         callback();
                                     }
                                 }
-                            );
+                            )
                         },
-                        removeListener: (id, $element) => {
+                        removeListener: (id) => {
                             $(document).off(
-                                methods.generateNamespacedEvent('click', id)
+                                methods.generateNamespacedEvent('keydown', id)
                             );
                         }
                     }
-                },
-                tabKey: {
-                    leavingFromFirstFocusableElement: (e, $focusableItems) => {
-                        return (document.activeElement === $focusableItems[0])
-                            && e.shiftKey;
-                    },
-                    leavingFromLastFocusableElement: (e, $focusableItems) => {
-                        return (document.activeElement === $focusableItems[$focusableItems.length - 1])
-                            && !e.shiftKey;
-                    },
-                    tabToCorrectElement: (e, $element, $focusableItems) => {
-                        if (methods.tabKey.leavingFromFirstFocusableElement(e, $focusableItems)) {
-                            e.preventDefault();
-                            $focusableItems[$focusableItems.length - 1].focus();
-                        } else if (methods.tabKey.leavingFromLastFocusableElement(e, $focusableItems)) {
-                            e.preventDefault();
-                            $focusableItems[0].focus();
-                        }
-                    },
-                    trap: (id, $element) => {
-                        let $focusableItems = $element.find(':focusable');  // Need jQuery UI for this
-                        $focusableItems[0].focus();
-
-                        $element.on(
-                            methods.generateNamespacedEvent('keydown', id),
-                            (e) => {
-                                if (e.keyCode === 9) {
-                                    methods.tabKey.tabToCorrectElement(e, $element, $focusableItems);
-                                }
-                            }
-                        );
-                    },
-                    removeTrap: (id, $element) => {
-                        $element.off(
-                            methods.generateNamespacedEvent('keydown', id)
-                        );
-                    }
-                },
-                escapeKey: {
-                    createListener: (id, callback) => {
-                        $(document).on(
-                            methods.generateNamespacedEvent('keydown', id),
-                            (e) => {
-                                if (e.keyCode === 27) {
-                                    callback();
-                                }
-                            }
-                        )
-                    },
-                    removeListener: (id) => {
-                        $(document).off(
-                            methods.generateNamespacedEvent('keydown', id)
-                        );
-                    }
-                }
-            };
-
+                };
+        
             return {
                 createEscapeKeyListener: methods.escapeKey.createListener,
                 removeEscapeKeyListener: methods.escapeKey.removeListener,
