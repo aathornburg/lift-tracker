98d962b0e2c35e3f518eb13ccd2b4a7a51da0c85
aathornburg <alan.thornb@gmail.com>
2018-09-23 17:54:13 -0400

    Made basic workouts page and updated add lift modal

---
+++ src/app/pages/workouts/workouts.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WorkoutsComponent } from './components/workouts/workouts.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [WorkoutsComponent]
+})
+export class WorkoutsModule { }
