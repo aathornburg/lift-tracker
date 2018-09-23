b846c46958165e28c2638a20ad7ea7af7ef521ce
aathornburg <alan.thornb@gmail.com>
2018-09-23 18:45:11 -0400

    Fixed naming of exported const for step animation

--- src/app/shared/step/components/step-group/step-group.component.ts
+++ src/app/shared/step/components/step-group/step-group.component.ts
@@ -1,13 +1,13 @@
 import { Component, AfterContentInit, ContentChildren, QueryList, Input } from '@angular/core';
 import { StepService } from '../../services/step.service';
 import { StepComponent } from '../step/step.component';
-import { SlideLeftRight } from '../../step.animations';
+import { slideLeftRight } from '../../step.animations';
 
 @Component({
   selector: 'lt-step-group',
   templateUrl: './step-group.component.html',
   styleUrls: ['./step-group.component.scss'],
-  animations: [ SlideLeftRight ]
+  animations: [ slideLeftRight ]
 })
 export class StepGroupComponent implements AfterContentInit {
 
--- src/app/shared/step/step.animations.ts
+++ src/app/shared/step/step.animations.ts
@@ -1,6 +1,6 @@
 import { trigger, state, style, animate, transition } from '@angular/animations';
 
-export const SlideLeftRight = trigger('slideLeftRight', [
+export const slideLeftRight = trigger('slideLeftRight', [
     state('*', style({transform: 'translateX({{indexOffset}}%)'}), { params: { indexOffset: 0 }}),
     transition('* <=> *', [
         animate('300ms ease-in-out')
