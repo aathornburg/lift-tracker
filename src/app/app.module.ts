5ec256a07cba6561b28f258c2b06b048e0eeed4c
aathornburg <alan.thornb@gmail.com>
2018-10-01 19:41:49 -0400

    Added reactive forms to app.  Hid workout label when text is in input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CoreModule } from './core/core.module';
 
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     HttpClientModule,
     RouterModule,
+    ReactiveFormsModule,
     CoreModule
   ],
   bootstrap: [AppComponent]
--- src/app/pages/workouts/components/workouts/workouts.component.ts
+++ src/app/pages/workouts/components/workouts/workouts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'lt-workouts',
@@ -7,11 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkoutsComponent implements OnInit {
 
-  trackWorkoutStepsIdentifier: string = "track-workout-steps";
+  trackWorkoutStepsIdentifier = 'track-workout-steps';
+  trackWorkoutForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(): void {
+    this.trackWorkoutForm = this.formBuilder.group({
+      workoutName: ''
+    });
   }
 
 }
